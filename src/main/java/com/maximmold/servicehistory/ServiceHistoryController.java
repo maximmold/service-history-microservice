9838624fdf6911300ccbf9d9e4571f9dec42c32d
Maxim Moldenhauer <maximmold@gmail.com>
2018-03-06 08:45:18 -0500

    Fix PathVariable

--- src/main/java/com/maximmold/servicehistory/ServiceHistoryController.java
+++ src/main/java/com/maximmold/servicehistory/ServiceHistoryController.java
@@ -1,6 +1,7 @@
 package com.maximmold.servicehistory;
 
 import io.github.benas.randombeans.api.EnhancedRandom;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +13,8 @@ public class ServiceHistoryController {
 
     private Map<Long, ServiceHistory> serviceHistoryHashMap = new ConcurrentHashMap<>();
     
-    @RequestMapping("serviceHistory")
-    public ServiceHistory serviceHistory(Long fileNumber){
+    @RequestMapping("serviceHistory/{fileNumber}")
+    public ServiceHistory serviceHistory(@PathVariable Long fileNumber){
         ServiceHistory serviceHistory = serviceHistoryHashMap.get(fileNumber);
         if (serviceHistory != null) {
             serviceHistory = EnhancedRandom.random(ServiceHistory.class);
